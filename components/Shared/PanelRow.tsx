bc6e3413e0350ecc94d12164617aa5a5e8117e12
Jesus Cabrita <jesusarnaldo115@gmail.com>
2023-05-25 20:51:15 -0300

    front:cambios

--- components/Shared/PanelRow.tsx
+++ components/Shared/PanelRow.tsx
@@ -14,7 +14,7 @@ import { jugadoresPut_gol } from '../../service/jugadores';
 import { alertaSubmit } from '../../utils/alert';
 import { status } from '../../utils/utils';
 
-export const PanelRow =({homeTeam, awayTeam,currentRound, isLoading, index})=>{
+export const PanelRow = ({ homeTeam, awayTeam, currentRound, isLoading, index }) => {
     const mobile = useMediaQuery("(max-width:600px)", { noSsr: true });
     const [light] = useContext(Context);
     const [showImage, setShowImage] = useState(false);
@@ -34,6 +34,8 @@ export const PanelRow =({homeTeam, awayTeam,currentRound, isLoading, index})=>{
     const { mutate: editarGol } = useMutation(jugadoresPut_gol);
     const queryClient = useQueryClient();
     const [isLoadinng, setIsLoadinng] = useState(false);
+
+    const [goles, setGoles] = useState(0);
 
     useEffect(() => {
         const timer = setInterval(() => {
@@ -59,12 +61,28 @@ export const PanelRow =({homeTeam, awayTeam,currentRound, isLoading, index})=>{
         }
     }, [isLoading]);
 
-    const editarArbitros =(equipoId: string, jugadorId: string, gol_partido: number, index, jugador_gol, jugador_name)=>{
+    const editarGoles = (equipoId: string, jugadorId: string, gol_partido: number, index: number, jugador_gol: number, jugador_name: string, currentIndex: number, goles: number, equipo, golesAFavor: number) => {
         setIsLoadinng(true);
-        let updatedGolpartido =jugador_gol += gol_partido;
-        let updatedGolpartidoArr = [...homeTeam.gol_partido]; 
-        updatedGolpartidoArr[index] = updatedGolpartido; 
-        const formData = {gol_partido: updatedGolpartidoArr };
+        let updatedGolpartidoArr = [...homeTeam.jugadores[currentIndex].gol_partido];
+        let updatedGolpartido = jugador_gol + gol_partido;
+        updatedGolpartidoArr[index] = updatedGolpartido;
+        
+        let sumaGolesaFavor = golesAFavor + gol_partido
+
+        let updatedGolEquipoArr = [...homeTeam.gol_partido];
+        let updatedGolEquipo = equipo + gol_partido;
+        updatedGolEquipoArr[index] = updatedGolEquipo;
+
+        let sumaGoles = goles + gol_partido;       
+        const formData = {
+            jugadores: {
+                gol_partido: updatedGolpartidoArr,
+                goles: sumaGoles,
+            },
+            gol_partido: updatedGolEquipoArr,
+            goles: sumaGoles,
+            goles_a_Favor: sumaGolesaFavor === null ? 0 : sumaGolesaFavor,
+        };
         editarGol({ form: formData, equipoId, jugadorId }, {
             onSuccess: (success) => {
                 queryClient.invalidateQueries(["/api/liga"]);
@@ -77,87 +95,90 @@ export const PanelRow =({homeTeam, awayTeam,currentRound, isLoading, index})=>{
                 setIsLoadinng(false);
             },
         });
-    }
+    };
 
-    return(
+
+    return (
         <>
-        <Grid item gap={!mobile ?4: 0} container width={'100%'} flexDirection={'row'} alignItems={'center'} sx={{ padding: '20px' }}>
-            <Grid item>
-                <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)} sx={{ color: light ? 'black' : 'var(--cero)' }}>
-                    {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                </IconButton>
-            </Grid>
-            <Grid item sx={{ color: light ? 'var(--dark2)' : 'var(--cero)'}}>
-                <Grid item width={!mobile ?'110px':'80px'} container flexDirection={'column'} alignItems={'center'} justifyContent={'center'}>
-                    <Grid item>Partido {index +1}</Grid>
-                    <Grid item sx={{fontSize:!mobile ?'16px':'14px'}}>{homeTeam?.fecha[currentRound] === 'No definido' ? 'No definido': formatoFecha}</Grid>
-                    <Grid item sx={{fontSize:!mobile ?'12px':'10px'}}>{homeTeam?.fecha[currentRound] === 'No definido' ? 'No definida': formatoHora}</Grid>
-                </Grid>
-            </Grid>
-            <Grid container width={!mobile ?'470px':'210px'} flexDirection={'row'} alignItems={'center'} sx={{color: light ? 'var(--dark2)' : 'var(--cero)'}}>
-                {!mobile &&
-                <Grid item container alignItems={'center'} justifyContent={'end'} sx={{ whiteSpace: 'nowrap', width: '130px'}}>
-                    {homeTeam?.name}
-                </Grid>}
-                <Grid item container alignItems={'center'} justifyContent={'center'} sx={{ width: '55px', height: '35px' }}>
-                    {isLoading || !showImage ?
-                        (<CircularProgress style={{ color: light ? 'var(--dark2)' : 'var(--cero)' }} size={20} />)
-                    : showImage ? <img style={{ height: '30px' }} src={homeTeam?.logo} alt={homeTeam?.name} />
-                    : null}
-                </Grid>
+            <Grid item gap={!mobile ? 4 : 0} container width={'100%'} flexDirection={'row'} alignItems={'center'} sx={{ padding: '20px' }}>
                 <Grid item>
-                    <ButtonStatus status={status(hoy,fechaFinalPartido,tiempoRestante,TIEMPO_PARTIDO)} gol_away={gol_away} gol_home={gol_home} minutosTranscurridos={minutosTranscurridos} />
+                    <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)} sx={{ color: light ? 'black' : 'var(--cero)' }}>
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
                 </Grid>
-                <Grid item container alignItems={'center'} justifyContent={'center'} sx={{ width: '55px', height: '35px' }}>
-                    {isLoading || !showImage ?
-                        (<CircularProgress style={{ color: light ? 'var(--dark2)' : 'var(--cero)' }} size={20} />)
-                    : showImage ? <img style={{ height: '30px' }} src={awayTeam?.logo} alt={awayTeam?.name} />
-                    : null}
+                <Grid item sx={{ color: light ? 'var(--dark2)' : 'var(--cero)' }}>
+                    <Grid item width={!mobile ? '110px' : '80px'} container flexDirection={'column'} alignItems={'center'} justifyContent={'center'}>
+                        <Grid item>Partido {index + 1}</Grid>
+                        <Grid item sx={{ fontSize: !mobile ? '16px' : '14px' }}>{homeTeam?.fecha[currentRound] === 'No definido' ? 'No definido' : formatoFecha}</Grid>
+                        <Grid item sx={{ fontSize: !mobile ? '12px' : '10px' }}>{homeTeam?.fecha[currentRound] === 'No definido' ? 'No definida' : formatoHora}</Grid>
+                    </Grid>
                 </Grid>
-                {!mobile &&
-                <Grid item container alignItems={'center'} sx={{ whiteSpace: 'nowrap', width: '130px' }}>
-                    {awayTeam?.name}
-                </Grid>}
-            </Grid>
-        </Grid>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-            <Grid p={5}> 
-                <Grid container gap={6}>
+                <Grid container width={!mobile ? '470px' : '210px'} flexDirection={'row'} alignItems={'center'} sx={{ color: light ? 'var(--dark2)' : 'var(--cero)' }}>
+                    {!mobile &&
+                        <Grid item container alignItems={'center'} justifyContent={'end'} sx={{ whiteSpace: 'nowrap', width: '130px' }}>
+                            {homeTeam?.name}
+                        </Grid>}
+                    <Grid item container alignItems={'center'} justifyContent={'center'} sx={{ width: '55px', height: '35px' }}>
+                        {isLoading || !showImage ?
+                            (<CircularProgress style={{ color: light ? 'var(--dark2)' : 'var(--cero)' }} size={20} />)
+                            : showImage ? <img style={{ height: '30px' }} src={homeTeam?.logo} alt={homeTeam?.name} />
+                                : null}
+                    </Grid>
                     <Grid item>
-                    <Grid mb={2} item sx={{background:'var(--primario)', borderRadius:'8px', display:'flex', alignItems:'center', justifyContent:'center', color:'var(--cero)', padding:'2px'}}>{homeTeam?.name}</Grid>
-                        {homeTeam?.jugadores.map((jugador) => {
-                            return (
-                                <>
-                                    <Grid item gap={2} container alignItems={'center'} sx={{ color: light ? 'var(--dark2)' : 'var(--gris)' }}>
-                                        <Grid item sx={{fontSize:'20px', display:'flex', alignItems:'center', justifyContent:'center', width:'40px', fontWeight:600}}>#{jugador.dorsal} </Grid>
-                                        <Grid item width={'150px'} sx={{cursor:'pointer'}}>{jugador.name} </Grid>
-                                        <Grid item sx={{cursor:'pointer'}} onClick={()=>{editarArbitros(homeTeam._id, jugador._id,1, currentRound, jugador.gol_partido[currentRound], jugador.name)}}><Gol/></Grid>
-                                        <Grid item sx={{cursor:'pointer'}}><Tarjeta color={'var(--warnning)'}/></Grid>
-                                        <Grid item sx={{cursor:'pointer'}}><Tarjeta color={'var(--danger)'}/></Grid>
-                                    </Grid> 
-                                </>
-                            )
-                        })}
+                        <ButtonStatus status={status(hoy, fechaFinalPartido, tiempoRestante, TIEMPO_PARTIDO)} gol_away={gol_away} gol_home={gol_home} minutosTranscurridos={minutosTranscurridos} />
                     </Grid>
-                    <Grid>
-                    <Grid mb={2} item sx={{background:'var(--check)', borderRadius:'8px', display:'flex', alignItems:'center', justifyContent:'center', color:'var(--cero)', padding:'2px'}}>{awayTeam?.name}</Grid>
-                        {awayTeam?.jugadores.map((jugador) => {
-                            return (
-                                <>
-                                    <Grid item gap={2} container alignItems={'center'} sx={{ color: light ? 'var(--dark2)' : 'var(--gris)' }}>
-                                        <Grid item sx={{fontSize:'20px', display:'flex', alignItems:'center', justifyContent:'center', width:'40px', fontWeight:600}}>#{jugador.dorsal} </Grid>
-                                        <Grid item width={'150px'} sx={{cursor:'pointer'}}>{jugador.name} </Grid>
-                                        <Grid item sx={{cursor:'pointer'}}><Gol/></Grid>
-                                        <Grid item sx={{cursor:'pointer'}}><Tarjeta color={'var(--warnning)'}/></Grid>
-                                        <Grid item sx={{cursor:'pointer'}}><Tarjeta color={'var(--danger)'}/></Grid>
-                                    </Grid> 
-                                </>
-                            )
-                        })}
+                    <Grid item container alignItems={'center'} justifyContent={'center'} sx={{ width: '55px', height: '35px' }}>
+                        {isLoading || !showImage ?
+                            (<CircularProgress style={{ color: light ? 'var(--dark2)' : 'var(--cero)' }} size={20} />)
+                            : showImage ? <img style={{ height: '30px' }} src={awayTeam?.logo} alt={awayTeam?.name} />
+                                : null}
                     </Grid>
+                    {!mobile &&
+                        <Grid item container alignItems={'center'} sx={{ whiteSpace: 'nowrap', width: '130px' }}>
+                            {awayTeam?.name}
+                        </Grid>}
                 </Grid>
             </Grid>
-    </Collapse>
-    </>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <Grid p={5}>
+                    <Grid container gap={6}>
+                        <Grid item>
+                            <Grid mb={2} item sx={{ background: 'var(--primario)', borderRadius: '8px', display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'var(--cero)', padding: '2px' }}>{homeTeam?.name}</Grid>
+                            {homeTeam?.jugadores.map((jugador, index) => {
+                                console.log('gola a favor :' ,jugador.goles_a_Favor);
+                                
+                                return (
+                                    <>
+                                        <Grid item gap={2} container alignItems={'center'} sx={{ color: light ? 'var(--dark2)' : 'var(--gris)' }}>
+                                            <Grid item sx={{ fontSize: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '40px', fontWeight: 600 }}>#{jugador.dorsal} </Grid>
+                                            <Grid item width={'150px'} sx={{ cursor: 'pointer' }}>{jugador.name} </Grid>
+                                            <Grid item sx={{ cursor: 'pointer' }} onClick={() => { editarGoles(homeTeam._id, jugador._id, 1, currentRound, jugador.gol_partido[currentRound], jugador.name, index, jugador.goles, homeTeam.gol_partido[currentRound],homeTeam.goles_a_Favor) }}><Gol /></Grid>
+                                            <Grid item sx={{ cursor: 'pointer' }}><Tarjeta color={'var(--warnning)'} /></Grid>
+                                            <Grid item sx={{ cursor: 'pointer' }}><Tarjeta color={'var(--danger)'} /></Grid>
+                                        </Grid>
+                                    </>
+                                )
+                            })}
+                        </Grid>
+                        <Grid>
+                            <Grid mb={2} item sx={{ background: 'var(--check)', borderRadius: '8px', display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'var(--cero)', padding: '2px' }}>{awayTeam?.name}</Grid>
+                            {awayTeam?.jugadores.map((jugador, index) => {
+                                return (
+                                    <>
+                                        <Grid item gap={2} container alignItems={'center'} sx={{ color: light ? 'var(--dark2)' : 'var(--gris)' }}>
+                                            <Grid item sx={{ fontSize: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '40px', fontWeight: 600 }}>#{jugador.dorsal} </Grid>
+                                            <Grid item width={'150px'} sx={{ cursor: 'pointer' }}>{jugador.name} </Grid>
+                                            <Grid item sx={{ cursor: 'pointer' }}><Gol /></Grid>
+                                            <Grid item sx={{ cursor: 'pointer' }}><Tarjeta color={'var(--warnning)'} /></Grid>
+                                            <Grid item sx={{ cursor: 'pointer' }}><Tarjeta color={'var(--danger)'} /></Grid>
+                                        </Grid>
+                                    </>
+                                )
+                            })}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Collapse>
+        </>
     )
 }
