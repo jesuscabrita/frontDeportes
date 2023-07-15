291f128f1aefb9b703294eceb82f91994e8e6a77
Jesus Cabrita <jesusarnaldo115@gmail.com>
2023-07-15 00:04:53 -0300

    front: cambios

---
+++ components/Material/InputPassword.tsx
@@ -0,0 +1,59 @@
+import { useContext, useState } from "react";
+import { Grid, TextField, InputAdornment, IconButton } from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Context from "../../context/contextPrincipal";
+
+export const InputPassword = ({ value, setValue, label, placeholder }) => {
+    const [light] = useContext(Context);
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <Grid item sx={{ width: "100%", display: "flex", flexDirection: "column", color: light ? "var(--dark2)" : "var(--cero)", fontSize: "10px" }}>
+            {label}
+            <TextField
+                type={showPassword ? "text" : "password"}
+                placeholder={placeholder}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                autoComplete="off"
+                sx={{
+                    "& .MuiOutlinedInput-input": {
+                        borderRadius: "6px",
+                        color: light ? "var(--dark2)" : "var(--cero)",
+                        outline: "none",
+                        width: "100%",
+                    },
+                    "& .MuiInputLabel-root": {
+                        color: light ? "var(--dark2)" : "var(--cero)",
+                    },
+                    "& .MuiOutlinedInput-root": {
+                        "& fieldset": {
+                            borderColor: light ? "var(--dark2)" : "var(--cero)",
+                        },
+                        "&:hover fieldset": {
+                            borderColor: light ? "var(--dark2)" : "var(--cero)",
+                        },
+                        "&.Mui-focused fieldset": {
+                            borderColor: light ? "var(--dark2)" : "var(--cero)",
+                        },
+                    },
+                }}
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                                onClick={() => setShowPassword(!showPassword)}
+                                sx={{ color: light ? "var(--dark2)" : "var(--cero)" }}
+                                edge="end"
+                                tabIndex={-1}
+                            >
+                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
+            />
+        </Grid>
+    );
+};
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,52 +1,62 @@
-import { Grid, CircularProgress } from "@mui/material";
+import { Grid, useMediaQuery, Typography, Paper, Avatar } from "@mui/material";
 import { useContext, useState } from "react";
-import { InputText } from "../components/Material/InputTex";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { useRouter } from 'next/router';
 import { ButtonSend } from "../components/Material/ButtonSend";
+import { InputPassword } from "../components/Material/InputPassword";
+import { InputText } from "../components/Material/InputTex";
 import ContextRefac from "../context/contextLogin";
+import Context from "../context/contextPrincipal";
 
 const Login = () => {
+    const mobile = useMediaQuery("(max-width:600px)", { noSsr: true });
+    const [light] = useContext(Context);
     const [emailOrUsername, setEmailOrUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
     const { state: { user }, SignIn }: any = useContext(ContextRefac);
-    
+    const router = useRouter();
+
     const handleLogin = async () => {
         await SignIn({ email: emailOrUsername, password });
     };
 
+    const navigateToForgotPassword = () => {
+        router.push("/forgot-password");
+    };
+
+    const navigateToRegister = () => {
+        router.push("/register");
+    };
+
     return (
-        <Grid
-            container
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            height="100vh"
-            gap={2}
-        >
-            <InputText
-                label={"Usuario"}
-                placeholder={"Email"}
-                setValue={setEmailOrUsername}
-                value={emailOrUsername}
-            />
-            <InputText
-                label={"Contraseña"}
-                placeholder={"Contraseña"}
-                setValue={setPassword}
-                value={password}
-            />
-            {isLoading ? (
-                <CircularProgress color="primary" />
-            ) : (
-                <ButtonSend
-                    disable={false}
-                    icon={""}
-                    iconColor={""}
-                    iconSize={20}
-                    title={"Iniciar sesión"}
-                    handle={handleLogin}
-                />
-            )}
+        <Grid container direction="column" alignItems="center" justifyContent="center" height="100vh" gap={2} style={{ padding: mobile ? "0 20px" : "0 50px" }}>
+            <Paper elevation={3} sx={{ padding: mobile ? "20px" : "40px", display: "flex", flexDirection: "column", alignItems: "center", background: light ? 'var(--gris)' : 'var(--dark2)' }}>
+                <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5" component="h1" gutterBottom sx={{ color: light ? "var(--dark2)" : "var(--cero)" }}>
+                    Iniciar sesión
+                </Typography>
+                <Grid item sx={{ width: '100%' }}>
+                    <InputText label="Usuario" placeholder="Email" setValue={setEmailOrUsername} value={emailOrUsername} />
+                </Grid>
+                <Grid item mt={2} sx={{ width: '100%' }}>
+                    <InputPassword label="Contraseña" placeholder="Contraseña" setValue={setPassword} value={password} />
+                </Grid>
+                <Grid item mt={2}>
+                    <ButtonSend disable={false} icon="" iconColor="" iconSize={20} title="Iniciar sesión" handle={handleLogin} />
+                </Grid>
+                <Grid item mt={2}>
+                    <Typography variant="body2" sx={{ color: light ? "var(--dark3)" : "var(--gris2)", cursor: 'pointer', textDecoration: 'underline' }} onClick={navigateToForgotPassword}>
+                        ¿Olvidaste tu contraseña?
+                    </Typography>
+                </Grid>
+                <Grid item mt={2}>
+                    <Typography variant="body2" sx={{ color: light ? "var(--dark3)" : "var(--gris2)", cursor: 'pointer', textDecoration: 'underline' }} onClick={navigateToRegister}>
+                        ¿No tienes una cuenta? Regístrate aquí
+                    </Typography>
+                </Grid>
+            </Paper>
         </Grid>
     );
 };
