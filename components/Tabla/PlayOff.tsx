4948cf3f87c8a761fd9de7469979b6d252f1d675
Jesus Cabrita <jesusarnaldo115@gmail.com>
2023-08-24 11:20:50 -0300

    front: play-offs

--- components/Tabla/PlayOff.tsx
+++ components/Tabla/PlayOff.tsx
@@ -1,6 +1,10 @@
-import { Grid } from "@mui/material"
+import { Grid, useMediaQuery } from "@mui/material"
+import { useContext } from "react";
+import Context from "../../context/contextPrincipal";
 
 export const PlayOff = ({ data }) => {
+    const mobile = useMediaQuery("(max-width:600px)", { noSsr: true });
+    const [light] = useContext(Context);
 
     const equiposCuartos = data.slice(0, 8);
     const generarEnfrentamientosCuartos = (equiposCuartos) => {
@@ -36,75 +40,77 @@ export const PlayOff = ({ data }) => {
     
 
     return (
-        <Grid mt={2} sx={{display:'flex', justifyContent:'space-between'}}>
+        <Grid mt={2} sx={{display:'flex', justifyContent:'space-between',width:'100%'}}>
             <Grid item sx={{width:'100%'}}>
                 {enfrentamientosCuartos.slice(0, 2).map((enfrentamiento, index) => (
                     <Grid item sx={{display:'flex',alignItems:'center',width:'100%'}}>
                         <Grid item key={index} mb={5} sx={{width:'110px'}}>
                             <Grid item sx={{display:'flex',alignItems:'center', flexDirection:'column',}}>
-                                <img style={{ height: '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
-                                <Grid item sx={{color:'var(--neutral)', fontSize:'10px'}}>{enfrentamiento[0].name}</Grid>
-                                <Grid item sx={{color:'var(--check)', fontWeight:'700',display:'flex',alignItems:'center'}}>VS</Grid>
+                                <img style={{ height:mobile?'30px': '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
+                                <Grid item sx={{color:'var(--neutral)', fontSize:mobile?'7px':'10px'}}>{enfrentamiento[0].name}</Grid>
+                                <Grid item sx={{color:'var(--check)', fontWeight:'700',display:'flex',alignItems:'center',fontSize:mobile?'12px':'16px'}}>VS</Grid>
                             </Grid>
                             <Grid item sx={{display:'flex',alignItems:'center', flexDirection:'column',}}>
-                                <img style={{ height: '60px' }} src={enfrentamiento[1].logo} alt={enfrentamiento[1].name} />
-                                <Grid item sx={{color:'var(--neutral)', fontSize:'10px'}}>{enfrentamiento[1].name}</Grid>
+                                <img style={{ height:mobile?'30px': '60px' }} src={enfrentamiento[1].logo} alt={enfrentamiento[1].name} />
+                                <Grid item sx={{color:'var(--neutral)', fontSize:mobile?'7px':'10px'}}>{enfrentamiento[1].name}</Grid>
                             </Grid>
                         </Grid>
 
-                        <Grid item sx={{border:'1px red solid',width:'180px'}}></Grid>
-                        <Grid item sx={{borderLeft:'1px red solid',height:'60px',marginTop:index === 0 ?'60px':'-60px'}}></Grid>
-                        <Grid item sx={{borderBottom:index === 0 &&'1px red solid',borderTop:index === 1 &&'1px red solid' ,width:'20px',height:'60px',marginTop:index === 0 ?'60px':'-60px'}}></Grid>
+                        <Grid item sx={{border:light?'1px var(--dark) solid':'1px var(--cero) solid',width:mobile?'10px':'180px'}}></Grid>
+                        <Grid item sx={{borderLeft:light?'1px var(--dark) solid':'1px var(--cero) solid',height:'60px',marginTop:index === 0 ?'60px':'-60px'}}></Grid>
+                        {index === 0 && <Grid item sx={{borderBottom:light?'1px var(--dark) solid':'1px var(--cero) solid',height:'60px',marginTop:'60px'}}></Grid>}
                         
-                        <Grid mb={index === 0 ?-15:15} item sx={{display:'flex',alignItems:'center', flexDirection:'column',}}>
-                            <img style={{ height: '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
-                            <Grid item sx={{color:'var(--neutral)', fontSize:'10px'}}>{enfrentamiento[0].name}</Grid>
-                            {index === 0 && <Grid item sx={{color:'var(--check)', fontWeight:'700',display:'flex',alignItems:'center'}}>VS</Grid>}
+                        <Grid mb={index === 0 ?-15:15} item sx={{display:'flex',alignItems:'center', flexDirection:'column',width:mobile?'80px': '100px'}}>
+                            <img style={{ height: mobile?'30px': '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
+                            <Grid item sx={{color:'var(--neutral)', fontSize:mobile?'7px':'10px'}}>{enfrentamiento[0].name}</Grid>
+                            {index === 0 && <Grid mt={mobile?3:0} item sx={{color:'var(--check)', fontWeight:'700',display:'flex',alignItems:'center',fontSize:mobile?'12px':'16px'}}>VS</Grid>}
                         </Grid>
-                        {index === 0 && <Grid item sx={{border:'1px red solid',width:'100px',marginTop:index === 0 ?'200px':'-200px'}}></Grid>}
-                        {index === 1 && <Grid item sx={{width:'200px',marginBottom:'200px'}}></Grid>}
+                        {index === 0 && <Grid item sx={{border:light?'1px var(--dark) solid':'1px var(--cero) solid',width:mobile?'10px':'100px',marginTop:index === 0 ?'200px':'-200px'}}></Grid>}
+                        {index === 1 && <Grid item sx={{width:mobile?'80px':'200px',marginBottom:'200px'}}></Grid>}
                         {index === 0 &&
-                        <Grid mb={-25} item sx={{display:'flex',alignItems:'center', flexDirection:'column'}}>
-                            <img style={{ height: '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
-                            <Grid item sx={{color:'var(--neutral)', fontSize:'10px'}}>{enfrentamiento[0].name}</Grid>
+                        <Grid mb={-25} item sx={{display:'flex',alignItems:'center', flexDirection:'column',width:mobile?'80px': '100px'}}>
+                            <img style={{ height:mobile?'30px': '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
+                            <Grid item sx={{color:'var(--neutral)', fontSize:mobile?'7px':'10px'}}>{enfrentamiento[0].name}</Grid>
                         </Grid>}
                     </Grid>
                 ))}               
             </Grid>
-            <Grid item  sx={{display:'flex',alignItems:'center', flexDirection:'column', justifyContent:'center'}}>
-                <img style={{ height: '60px' }} src={enfrentamientosCuartos.slice(0, 2)[0][0].logo} alt={enfrentamientosCuartos.slice(0, 2)[0][0].name} />
-                <Grid mb={10} item sx={{color:'var(--neutral)', fontSize:'10px'}}>{enfrentamientosCuartos.slice(0, 2)[0][0].name}</Grid>
-                <Grid mb={25} item sx={{color:'var(--check)', fontWeight:'700',display:'flex',alignItems:'center'}}>VS</Grid>
+            <Grid item  sx={{display:'flex',alignItems:'center', flexDirection:'column', justifyContent:'center',width:mobile?'80px': '100%'}}>
+                <Grid item sx={{color:'var(--check)', fontWeight:'700',display:'flex',alignItems:'center',fontSize:mobile?'12px':'16px'}}>CAMPEONES</Grid>
+                <img style={{ height: mobile?'30px': '60px' }} src={enfrentamientosCuartos.slice(0, 2)[0][0]?.logo} alt={enfrentamientosCuartos.slice(0, 2)[0][0]?.name} />
+                <Grid mb={mobile?8:15} item sx={{color:'var(--neutral)', fontSize:mobile?'7px':'10px',whiteSpace: 'nowrap' }}>{enfrentamientosCuartos.slice(0, 2)[0][0]?.name}</Grid>
+                <Grid mb={mobile?14:25} item sx={{color:'var(--check)', fontWeight:'700',display:'flex',alignItems:'center',fontSize:mobile?'12px':'16px'}}>VS</Grid>
             </Grid>
             <Grid item sx={{width:'100%'}}>
                 {enfrentamientosCuartos.slice(0, 2).map((enfrentamiento, index) => (
                     <Grid item sx={{display:'flex',alignItems:'center',width:'100%'}}>
                         {index === 0 &&
-                        <Grid mb={-25} item sx={{display:'flex',alignItems:'center', flexDirection:'column',}}>
-                            <img style={{ height: '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
-                            <Grid item sx={{color:'var(--neutral)', fontSize:'10px'}}>{enfrentamiento[0].name}</Grid>
+                        <Grid mb={-25} item sx={{display:'flex',alignItems:'center', flexDirection:'column',width:mobile?'80px': '100px'}}>
+                            <img style={{ height: mobile?'30px': '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
+                            <Grid item sx={{color:'var(--neutral)', fontSize:mobile?'7px':'10px'}}>{enfrentamiento[0].name}</Grid>
                         </Grid>}
-                        {index === 0 && <Grid item sx={{border:'1px red solid',width:'100px',marginTop:index === 0 ?'200px':'-200px'}}></Grid>}
-                        {index === 1 && <Grid item sx={{width:'200px',marginBottom:'200px'}}></Grid>}
-                        <Grid mb={index === 0 ?-15:15} item sx={{display:'flex',alignItems:'center', flexDirection:'column',}}>
-                            <img style={{ height: '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
-                            <Grid item sx={{color:'var(--neutral)', fontSize:'10px'}}>{enfrentamiento[0].name}</Grid>
-                            {index === 0 && <Grid item sx={{color:'var(--check)', fontWeight:'700',display:'flex',alignItems:'center'}}>VS</Grid>}
+                        {index === 0 && <Grid item sx={{border:light?'1px var(--dark) solid':'1px var(--cero) solid',width:mobile?'10px':'100px',marginTop:index === 0 ?'200px':'-200px'}}></Grid>}
+                        {index === 1 && <Grid item sx={{width:mobile?'90px':'200px',marginBottom:'200px'}}></Grid>}
+                        <Grid mb={index === 0 ?-15:15} item sx={{display:'flex',alignItems:'center', flexDirection:'column',width:mobile?'80px': '100px'}}>
+                            <img style={{ height: mobile?'30px': '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
+                            <Grid item sx={{color:'var(--neutral)', fontSize:mobile?'7px':'10px'}}>{enfrentamiento[0].name}</Grid>
+                            {index === 0 && <Grid mt={mobile?3:0} item sx={{color:'var(--check)', fontWeight:'700',display:'flex',alignItems:'center',fontSize:mobile?'12px':'16px'}}>VS</Grid>}
                         </Grid>
 
-                        <Grid item sx={{borderBottom:index === 0 &&'1px red solid',borderTop:index === 1 &&'1px red solid' ,width:'20px',height:'60px',marginTop:index === 0 ?'60px':'-60px'}}></Grid>
-                        <Grid item sx={{borderRight:'1px red solid',height:'60px',marginTop:index === 0 ?'60px':'-60px'}}></Grid>
-                        <Grid item sx={{border:'1px red solid',width:'180px'}}></Grid>
+                        {index === 0 && <Grid item sx={{borderBottom:light?'1px var(--dark) solid':'1px var(--cero) solid',width:mobile?'10px':'20px',height:'60px',marginTop:'60px'}}></Grid>}
+                        {index === 1 && <Grid item sx={{borderTop:light?'1px var(--dark) solid':'1px var(--cero) solid',width:mobile?'10px':'20px',height:'60px',marginTop:'-60px'}}></Grid>}
+                        <Grid item sx={{borderRight:light?'1px var(--dark) solid':'1px var(--cero) solid',height:'60px',marginTop:index === 0 ?'60px':'-60px'}}></Grid>
+                        <Grid item sx={{border:light?'1px var(--dark) solid':'1px var(--cero) solid',width:mobile?'10px':'180px'}}></Grid>
 
                         <Grid item key={index} mb={5} sx={{width:'110px'}}>
-                            <Grid item sx={{display:'flex',alignItems:'center', flexDirection:'column',}}>
-                                <img style={{ height: '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
-                                <Grid item sx={{color:'var(--neutral)', fontSize:'10px'}}>{enfrentamiento[0].name}</Grid>
+                            <Grid item sx={{display:'flex',alignItems:'center', flexDirection:'column',width:mobile?'80px': '100px'}}>
+                                <img style={{ height: mobile?'30px': '60px' }} src={enfrentamiento[0].logo} alt={enfrentamiento[0].name} />
+                                <Grid item sx={{color:'var(--neutral)', fontSize:mobile?'7px':'10px'}}>{enfrentamiento[0].name}</Grid>
                                 <Grid item sx={{color:'var(--check)', fontWeight:'700',display:'flex',alignItems:'center'}}>VS</Grid>
                             </Grid>
-                            <Grid item sx={{display:'flex',alignItems:'center', flexDirection:'column',}}>
-                                <img style={{ height: '60px' }} src={enfrentamiento[1].logo} alt={enfrentamiento[1].name} />
-                                <Grid item sx={{color:'var(--neutral)', fontSize:'10px'}}>{enfrentamiento[1].name}</Grid>
+                            <Grid item sx={{display:'flex',alignItems:'center', flexDirection:'column',width:mobile?'80px': '100px'}}>
+                                <img style={{ height: mobile?'30px': '60px' }} src={enfrentamiento[1].logo} alt={enfrentamiento[1].name} />
+                                <Grid item sx={{color:'var(--neutral)', fontSize:mobile?'7px':'10px'}}>{enfrentamiento[1].name}</Grid>
                             </Grid>
                         </Grid>
                     </Grid>
